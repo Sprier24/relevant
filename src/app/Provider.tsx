b8c0c8de30794b2eab30507f8057bcec57b043d9
Sprier24 <spriersecure@gmail.com>
2025-09-26 17:59:00 +0530

    Delete src directory

--- src/app/Provider.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { AuthProvider } from '../app/context/AuthContext'; // update path accordingly
-
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <AuthProvider>{children}</AuthProvider>;
-}
